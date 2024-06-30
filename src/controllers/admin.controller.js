2429d207d334ddd1b63fd8d04bde461ee8b27852
pingwin <pingwin@MacBook-Air-Medvedev.local>
2024-07-01 04:37:37 +0700

    fix: lastes

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -6,5 +6,9 @@ const router = new Router()
 router.get('/getUsers', adminService.allUser)
 router.get('/getRoles', adminService.allRole)
 router.get('/getTransactions', adminService.allTransactions)
+router.get('/getAddresses', adminService.allAddresses)
+router.get('/getReviews', adminService.allReviews)
+router.get('/getProducts', adminService.allProducts)
+router.get('/getOrders', adminService.allOrder)
 
 module.exports = router;
--- src/services/admin.service.js
+++ src/services/admin.service.js
@@ -1,104 +1,283 @@
-const { User, Transaction, Role } = require("../models/model");
+const { User, Transaction, DeliveryAdress, Review, Role, Products, Category, Order } = require("../models/model");
 require("../models/model");
 
 class adminService {
-  async allUser(req, res) {
-	try{
-		const user = await User.findAll({
-			include: {
-				model: Role,
-				attributes: [ 'role_name' ]
+	async allUser(req, res) {
+		try{
+			const user = await User.findAll({
+				include: {
+					model: Role,
+					attributes: [ 'role_name' ]
+				},
+				attributes: [ 'id', 'login', 'password', 'email',],	
+			});
+			if(!user) return res.json({status_code: "bad", msg: "user not exists"})
+				
+		const columns = [ 
+			{ 
+				Header: '№', 
+				accessor: 'id'
 			},
-			attributes: [ 'id', 'login', 'password', 'email',],	
-		});
-		if(!user) return res.json({status_code: "bad", msg: "user not exists"})
-            
-    const columns = [ 
-        { 
-            Header: '№', 
-            accessor: 'id'
-        },
-        { 
-            Header: 'Имя пользователя',
-            accessor: 'login'
-        }, 
-        {
-            Header: 'Пароль',
-            accessor: 'password'
-        }, 
-        { 
-            Header: 'Email', 
-            accessor: 'email'
-        },
-        {
-            Header: 'Роль',
-            accessor: 'role.role_name'
-        }
-    ]
+			{ 
+				Header: 'Имя пользователя',
+				accessor: 'login'
+			}, 
+			{
+				Header: 'Пароль',
+				accessor: 'password'
+			}, 
+			{ 
+				Header: 'Email', 
+				accessor: 'email'
+			},
+			{
+				Header: 'Роль',
+				accessor: 'role.role_name'
+			}
+		]
 
-		return res.json({ columns: columns, users: user})
+			return res.json({ columns: columns, users: user})
+		}
+		catch(error) {
+			console.log(error)
+		}
 	}
-	catch(error) {
-		console.log(error)
-	}
-  }
 
-  async allRole(req, res) {
+	async allRole(req, res) {
+		try{
+			const role = await Role.findAll({
+				attributes: [ 'id', 'role_name'],	
+			});
+			if(!role) return res.json({status_code: "bad", msg: "user not exists"})
+		
+		const columns = [ 
+		{ 
+			Header: '№', 
+			accessor: 'id'
+		},
+		{ 
+			Header: 'Имя роли',
+			accessor: 'role_name'
+		}
+		]
+
+			return res.json({ columns: columns, users: role})
+		}
+		catch(error) {
+			console.log(error)
+		}
+	}
+
+	async allTransactions(req, res) {
+		try{
+			const transaction = await Transaction.findAll({
+				include: {
+					model: User,
+					attributes: [ 'login' ]
+				},
+				attributes: [ 'id', 'count', 'type', 'date' ],	
+			});
+			if(!transaction) return res.json({status_code: "bad", msg: "user not exists"})
+		
+			const columns = [ 
+			{ 
+				Header: '№', 
+				accessor: 'id'
+			},
+			{ 
+				Header: 'Сумма',
+				accessor: 'count'
+			},
+			{
+				Header: 'Тип транзакции', 
+				accessor: 'type'
+			},
+			{
+				Header: 'Дата',
+				accessor: 'date'
+			},
+			{
+				Header: "Имя пользователя",
+				accessor: 'user.login'
+			}
+			]
+
+			return res.json({ columns: columns, users: transaction})
+		}
+		catch(error) {
+			console.log(error)
+		}
+	}
+
+	async allAddresses(req, res) {
+		try{
+			const transaction = await DeliveryAdress.findAll({
+				include: {
+					model: User,
+					attributes: [ 'login' ]
+				},
+				attributes: [ 'id', 'adress'],	
+			});
+			if(!transaction) return res.json({status_code: "bad", msg: "user not exists"})
+		
+			const columns = [ 
+			{ 
+				Header: '№', 
+				accessor: 'id'
+			},
+			{ 
+				Header: 'Адрес',
+				accessor: 'adress'
+			},
+			{
+				Header: "Имя пользователя",
+				accessor: 'user.login'
+			}
+			]
+	
+			return res.json({ columns: columns, users: transaction})
+		}
+		catch(error) {
+			console.log(error)
+		}
+	  }
+
+	async allReviews(req, res) {
 	try{
-		const role = await Role.findAll({
-			attributes: [ 'id', 'role_name'],	
+		const review = await Review.findAll({
+			include: {
+				model: User,
+				attributes: [ 'login' ]
+			},
+			include: {
+				model: Products,
+				attributes: [ 'product_name' ]
+			},
+			attributes: [ 'id', 'name', 'surname', 'text', 'userId', 'productId'],	
 		});
-		if(!role) return res.json({status_code: "bad", msg: "user not exists"})
+		if(!review) return res.json({status_code: "bad", msg: "user not exists"})
 	
-        const columns = [ 
-        { 
-            Header: '№', 
-            accessor: 'id'
-        },
-        { 
-            Header: 'Имя роли',
-            accessor: 'role_name'
-        }
-    ]
+		const columns = [ 
+			{ 
+				Header: '№', 
+				accessor: 'id'
+			},
+			{ 
+				Header: 'Имя',
+				accessor: 'name'
+			},
+			{
+				Header: "Фамилия пользователя",
+				accessor: 'surname'
+			},
+			{
+				Header: "Текст отзыва",
+				accessor: 'text'
+			},
+			{
+				Header: "Логин пользователя",
+				accessor: 'user.login'
+			},
+			{
+				Header: "Название продукта",
+				accessor: 'product.product_name'
+			},
+		]
 
-		return res.json({ columns: columns, users: role})
+		return res.json({ columns: columns, users: review})
 	}
 	catch(error) {
 		console.log(error)
 	}
-  }
-
-  async allTransactions(req, res) {
-	try{
-		const transaction = await Transaction.findAll({
-            include: {
-                model: User,
-                attributes: [ 'login' ]
-            },
-			attributes: [ 'id', 'count', 'type', 'date' ],	
-		});
-		if(!transaction) return res.json({status_code: "bad", msg: "user not exists"})
-	
-		return res.json(transaction)
 	}
-	catch(error) {
-		console.log(error)
-	}
-  }
-
-//   async allCategories(req, res) {
-// 	try{
-// 		const role = await Role.findAll({
-// 			attributes: [ 'id', 'role_name'],	
-// 		});
-// 		if(!role) return res.json({status_code: "bad", msg: "user not exists"})
+	  
+	async allProducts(req, res) {
+		try{
+			const product = await Products.findAll({
+				
+				include: {
+					model: Category,
+					attributes: [ 'category_name' ]
+				},
+				attributes: [ 'id', 'product_name', 'price', 'value'],	
+			});
+			if(!product) return res.json({status_code: "bad", msg: "user not exists"})
+		
+			const columns = [ 
+				{ 
+					Header: '№', 
+					accessor: 'id'
+				},
+				{ 
+					Header: 'Название продукта',
+					accessor: 'product_name'
+				},
+				{
+					Header: "Цена",
+					accessor: 'price'
+				},
+				{
+					Header: "Количество на складе",
+					accessor: 'value'
+				},
+				{
+					Header: "Категория",
+					accessor: 'category.login'
+				},
+			]
 	
-// 		return res.json(role)
-// 	}
-// 	catch(error) {
-// 		console.log(error)
-// 	}
-//   }
-}
+			return res.json({ columns: columns, users: product})
+		}
+		catch(error) {
+			console.log(error)
+		}
+	  }
+
+	  async allOrder(req, res) {
+		try{
+			const product = await Order.findAll({
+				
+				include: {
+					model: User,
+					attributes: [ 'login' ]
+				},
+				include: {
+					model: DeliveryAdress,
+					attributes: [ 'adress' ]
+				},
+				attributes: [ 'id', 'statusOrder', 'owner', 'date'],	
+			});
+			if(!product) return res.json({status_code: "bad", msg: "user not exists"})
+		
+			const columns = [ 
+				{ 
+					Header: '№', 
+					accessor: 'id'
+				},
+				{ 
+					Header: 'Статус заказа',
+					accessor: 'statusOrder'
+				},
+				{
+					Header: "Контрагент",
+					accessor: 'user.login'
+				},
+				{
+					Header: "Дата",
+					accessor: 'value'
+				},
+				{
+					Header: "Адрес",
+					accessor: 'addressDeliveries.adress'
+				},
+			]
+	
+			return res.json({ columns: columns, users: product})
+		}
+		catch(error) {
+			console.log(error)
+		}
+	  }
+	}
 
 module.exports = new adminService()
