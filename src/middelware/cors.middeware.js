d73c8fa5e53155907f085f9f978f25ae987d9c40
pingwin <pingwin@MacBook-Air-Medvedev.local>
2024-07-02 02:58:51 +0700

    feat: fixed second cors

--- src/middelware/cors.middeware.js
+++ src/middelware/cors.middeware.js
@@ -2,6 +2,8 @@ function cors(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Content-Type", "application/json");
+    res.Header("Content-Type", "application/json");
     next();
 }
 
