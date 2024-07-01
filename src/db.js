93ff3c36580ff57a2b0166044820acdaaa2feb68
pingwin <pingwin@MacBook-Air-Medvedev.local>
2024-07-02 02:45:41 +0700

    fix: db.js I change path on server db

--- src/db.js
+++ src/db.js
@@ -1,3 +1,3 @@
 const {Sequelize} = require('sequelize');
 
-module.exports = new Sequelize('postgres://postgres:12345@localhost:5432/DominiDB')
+module.exports = new Sequelize('postgres://postgre:@>uX0JS04DK8aomwSGbA$oG=@79.174.88.130:17689/DominiDB')
